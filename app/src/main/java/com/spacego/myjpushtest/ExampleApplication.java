61eaa46f4fbc0432529701881ee2476870855437
lixiulia <xiuliang.li@gi-de.com>
2019-02-27 10:27:51 +0800

    Add user-defined broadcast receiver

--- app/src/main/java/com/spacego/myjpushtest/ExampleApplication.java
+++ app/src/main/java/com/spacego/myjpushtest/ExampleApplication.java
@@ -1,13 +1,19 @@
 package com.spacego.myjpushtest;
 
 import android.app.Application;
+import android.util.Log;
+
 import cn.jpush.android.api.JPushInterface;
 
 public class ExampleApplication extends Application {
+    public static String registrationID;
+
     @Override
     public void onCreate() {
         super.onCreate();
         JPushInterface.setDebugMode(true);
         JPushInterface.init(this);
+        registrationID = JPushInterface.getRegistrationID(this);
+        Log.d("TAG", "接收Registration Id : " + registrationID);
     }
 }
